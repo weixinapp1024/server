9a603972041e884b019b94de620b23dec1c338e0
zhangshuo <zhangshuolove@live.com>
2018-04-08 13:20:48 +0800

    项目基本文件

---
+++ src/main/java/weixin/controller/IndexController.java
@@ -0,0 +1,14 @@
+package weixin.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/home")
+
+public class IndexController {
+    @RequestMapping("/index")
+    public String index(){
+        return "index";
+    }
+}
---
+++ src/main/webapp/WEB-INF/view/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: 00index
+  Date: 2018/4/8
+  Time: 13:16
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+Hello World
+</body>
+</html>
