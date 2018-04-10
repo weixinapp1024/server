b747c3729fe7fb4815fab442482d833e3024371f
zhangshuo <zhangshuolove@live.com>
2018-04-10 13:10:26 +0800

    11

--- src/main/java/weixin/controller/IndexController.java
+++ src/main/java/weixin/controller/IndexController.java
@@ -1,14 +1,20 @@
 package weixin.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
 
 @Controller
 @RequestMapping("/")
 
 public class IndexController {
-    @RequestMapping("/")
-    public String index(){
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String index(ModelMap model){
+        model.addAttribute("tittle", "SpringMVC");
+        model.addAttribute("content", "Hello SpringMVC");
         return "index";
     }
+
 }
